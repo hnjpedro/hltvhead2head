4f55f1698fd85a74d4efa42f769f9033d0668c81
hnjpedro <pedro.nunesjorge@gmail.com>
2022-09-22 04:25:03 -0300

    added overlay to the whole page when modal is open

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const majorPopup = document.getElementById("major-popup");
 const majorMainPopup = document.getElementById("major-main-popup");
 const bigEventsPopup = document.getElementById("big-events-popup");
 const bigEventsMainPopup = document.getElementById("big-events-main-popup");
-const overlay = document.getElementById("popup-overlay");
+const overlay = document.getElementById('popup-overlay')
 const body = document.body;
 
 const auxOpenPopup = () => {
   body.style.overflow = "hidden";
   window.scroll(0, 0);
-  overlay.style.display = "block";
+  overlay.style.display = "block"
 };
 
 // MAJOR POPUP
@@ -24,48 +24,49 @@ majorButton.addEventListener("click", () => {
 
 majorPopup.addEventListener("click", (event) => {
   const classNameOfClickedElement = event.target.classList[0];
-  const classNames = ["close-btn", "popup-overlay"];
+  const classNames = ["close-btn", "popup"];
+  console.log(classNameOfClickedElement)
   const shouldClosePopUp = classNames.some(
     (className) => className === classNameOfClickedElement
   );
 
   if (shouldClosePopUp) {
     if (window.innerWidth < 1600) {
-      majorMainPopup.style.cssText =
-        "animation:slide-out .5s; animation-fill-mode: forwards";
+    majorMainPopup.style.cssText =
+      "animation:slide-out .5s; animation-fill-mode: forwards"
     } else {
       majorMainPopup.style.cssText =
-        "animation:slide-out200 .5s; animation-fill-mode: forwards";
+      "animation:slide-out200 .5s; animation-fill-mode: forwards" 
     }
     setTimeout(() => {
       majorPopup.style.display = "none";
-      overlay.style.display = "none";
     }, 500);
   }
   body.style.overflow = "auto";
+  overlay.style.display = 'none'
 });
 
 // BIG EVENTS POPUP
 bigEventsButton.addEventListener("click", () => {
-  bigEventsPopup.style.display = "flex";
-  bigEventsMainPopup.style.cssText =
-    "animation:slide-in .5s; animation-fill-mode: forwards";
-  auxOpenPopup();
-});
-
-bigEventsPopup.addEventListener("click", (event) => {
-  const classNameOfClickedElement = event.target.classList[0];
-  const classNames = ["close-btn", "popup-overlay"];
-  const shouldClosePopUp = classNames.some(
-    (className) => className === classNameOfClickedElement
-  );
-
-  if (shouldClosePopUp) {
+    bigEventsPopup.style.display = "flex";
     bigEventsMainPopup.style.cssText =
-      "animation:slide-out .5s; animation-fill-mode: forwards";
-    setTimeout(() => {
-      bigEventsPopup.style.display = "none";
-    }, 500);
-  }
-  body.style.overflow = "auto";
-});
+      "animation:slide-in .5s; animation-fill-mode: forwards";
+    auxOpenPopup();
+  });
+  
+  bigEventsPopup.addEventListener("click", (event) => {
+    const classNameOfClickedElement = event.target.classList[0];
+    const classNames = ["close-btn", "popup-overlay"];
+    const shouldClosePopUp = classNames.some(
+      (className) => className === classNameOfClickedElement
+    );
+  
+    if (shouldClosePopUp) {
+      bigEventsMainPopup.style.cssText =
+        "animation:slide-out .5s; animation-fill-mode: forwards";
+      setTimeout(() => {
+        bigEventsPopup.style.display = "none";
+      }, 500);
+    }
+    body.style.overflow = "auto";
+  });
