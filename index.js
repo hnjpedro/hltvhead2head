7b6322bc6367e518ed304ea76bc550fa535d04b3
hnjpedro <pedro.nunesjorge@gmail.com>
2022-09-23 00:23:05 -0300

    make page scroll back to position after closing modal, added smooth scrolling

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const majorPopup = document.getElementById("major-popup");
 const majorMainPopup = document.getElementById("major-main-popup");
 const bigEventsPopup = document.getElementById("big-events-popup");
 const bigEventsMainPopup = document.getElementById("big-events-main-popup");
+let scrollPos = [];
 let overlay = document.getElementById('popup-overlay')
 
 const body = document.body;
 
 const auxOpenPopup = () => {
+  scrollPos.push(window.pageYOffset);
   body.style.overflow = "hidden";
-  window.scroll(0, 0);
+  window.scrollTo(0, 0);
   overlay.style.display = 'block'
 };
 
@@ -43,6 +45,7 @@ majorPopup.addEventListener("click", (event) => {
       majorPopup.style.display = "none";
       body.style.overflow = "auto";
       overlay.style.display = "none";
+      window.scrollTo(0,scrollPos.slice(-1))
     }, 500);
   }
 });
@@ -74,6 +77,7 @@ bigEventsPopup.addEventListener("click", (event) => {
       bigEventsPopup.style.display = "none";
       body.style.overflow = "auto";
       overlay.style.display = "none";
+      window.scrollTo(0,scrollPos.slice(-1))
     }, 500);
   }
 });
