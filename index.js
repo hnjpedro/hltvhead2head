00df2e9286c60799c3f99abf42d8e8963a2de562
hnjpedro <pedro.nunesjorge@gmail.com>
2022-09-25 16:32:09 -0300

    refactored js code, added ability to close modals using esc key

--- index.js
+++ index.js
@@ -1,42 +1,31 @@
-const majorButton = document.getElementById("stats-major");
-const bigEventsButton = document.getElementById("stats-big-events");
-const majorPopup = document.getElementById("major-popup");
-const majorMainPopup = document.getElementById("major-main-popup");
-const bigEventsPopup = document.getElementById("big-events-popup");
-const bigEventsMainPopup = document.getElementById("big-events-main-popup");
-let overlay = document.getElementById("popup-overlay");
+const overlay = document.getElementById("popup-overlay");
+const allPopups = document.querySelectorAll(".popup");
+const statsButtons = document.querySelectorAll(".open-btn");
+const mainPopup = document.querySelectorAll(".main-popup");
 const body = document.body;
 
 // TOOLTIP ICONS
-
-const info = () => {
-  var tooltips = document.querySelectorAll(".tooltip");
-  for (let i = 0; i < tooltips.length; ++i) {
-    var infoButton = document.createElement("span");
-    infoButton.innerHTML = `<span class='material-icons-outlined info-icon'>info</span>`;
-
-    while (infoButton.firstChild) {
-      tooltips[i].appendChild(infoButton.firstChild);
-    }
+const tooltips = document.querySelectorAll(".tooltip");
+for (let i = 0; i < tooltips.length; ++i) {
+  var infoButton = document.createElement("span");
+  infoButton.innerHTML = `<span class='material-icons-outlined info-icon'>info</span>`;
+  while (infoButton.firstChild) {
+    tooltips[i].appendChild(infoButton.firstChild);
   }
-};
-
-info();
+}
 
 // TOOLTIPS ON MOBILE
 if (window.innerWidth < 1000) {
   const tooltips = document.querySelectorAll("h4[title]");
-
   for (let i = 0; i < tooltips.length; ++i) {
     const auxTool = () => {
       const toolTipText = document.createElement("span");
-
       toolTipText.innerHTML =
         '<span class="title">' + tooltips[i].getAttribute("title") + "</span>";
       while (toolTipText.firstChild) {
         tooltips[i].appendChild(toolTipText.firstChild);
       }
-    }
+    };
     tooltips[i].addEventListener("click", (event) => {
       let activeTool = document.querySelectorAll(".title");
       if (!activeTool.length) {
@@ -56,93 +45,54 @@ if (window.innerWidth < 1000) {
   }
 }
 
-// }
-
-/* $(document).ready(function () {
-  $("h4[title]").click(function () {
-    var $titles = $(document).find(".title");
-    var $title = $(this).find(".title");
-    if (!$title.length) {
-      $titles.remove();
-      $(this).append(
-        '<span class="title">' + $(this).attr("title") + "</span>"
-      );
-    } else {
-      $title.remove();
-    }
+// OPEN POPUPS
+for (let i = 0; i < statsButtons.length; ++i) {
+  statsButtons[i].addEventListener("click", () => {
+    allPopups[i].style.display = "flex";
+    allPopups[i].style.top = `${window.pageYOffset + 10}px`;
+    mainPopup[i].style.cssText =
+      "animation:slide-in .5s; animation-fill-mode: forwards";
+    overlay.style.top = `${window.pageYOffset}px`;
+    body.style.overflow = "hidden";
+    overlay.style.display = "block";
   });
-}); */
+}
 
-const auxOpenPopup = () => {
-  const allPopups = document.querySelectorAll(".popup");
-  for (const popup of allPopups) {
-    popup.style.top = `${window.pageYOffset + 10}px`;
+// CLOSE POPUPS
+const closePopUpAux = () => {
+  for (let i = 0; i < allPopups.length; ++i) {
+    if (window.innerWidth < 1000 || window.innerWidth > 1600) {
+      mainPopup[i].style.cssText =
+        "animation:slide-out200 .5s; animation-fill-mode: forwards";
+    } else {
+      mainPopup[i].style.cssText =
+        "animation:slide-out .5s; animation-fill-mode: forwards";
+    }
+    setTimeout(() => {
+      allPopups[i].style.display = "none";
+      body.style.overflow = "auto";
+      overlay.style.display = "none";
+    }, 500);
   }
-  overlay.style.top = `${window.pageYOffset}px`;
-  body.style.overflow = "hidden";
-  overlay.style.display = "block";
 };
 
-// MAJOR POPUP
-
-majorButton.addEventListener("click", () => {
-  majorPopup.style.display = "flex";
-  majorMainPopup.style.cssText =
-    "animation:slide-in .5s; animation-fill-mode: forwards";
-  auxOpenPopup();
-});
-
-majorPopup.addEventListener("click", (event) => {
-  const classNameOfClickedElement = event.target.classList[0];
-  const classNames = ["close-btn", "popup", "popup-overlay"];
-  console.log(classNameOfClickedElement);
-  const shouldClosePopUp = classNames.some(
-    (className) => className === classNameOfClickedElement
-  );
-
-  if (shouldClosePopUp) {
-    if (window.innerWidth < 1000 || window.innerWidth > 1600) {
-      majorMainPopup.style.cssText =
-        "animation:slide-out200 .5s; animation-fill-mode: forwards";
-    } else {
-      majorMainPopup.style.cssText =
-        "animation:slide-out .5s; animation-fill-mode: forwards";
+for (let i = 0; i < allPopups.length; ++i) {
+  allPopups[i].addEventListener("click", (event) => {
+    const classNameOfClickedElement = event.target.classList[0];
+    const classNames = ["close-btn", "popup", "popup-overlay"];
+    console.log(classNameOfClickedElement);
+    const shouldClosePopUp = classNames.some(
+      (className) => className === classNameOfClickedElement
+    );
+    if (shouldClosePopUp) {
+      closePopUpAux();
     }
-    setTimeout(() => {
-      majorPopup.style.display = "none";
-      body.style.overflow = "auto";
-      overlay.style.display = "none";
-    }, 500);
+  });
+}
+
+/// CLOSE MODALS WITH ESC
+window.onkeydown = function (event) {
+  if (event.keyCode == 27) {
+    closePopUpAux();
   }
-});
-
-// BIG EVENTS POPUP
-bigEventsButton.addEventListener("click", () => {
-  bigEventsPopup.style.display = "flex";
-  bigEventsMainPopup.style.cssText =
-    "animation:slide-in .5s; animation-fill-mode: forwards";
-  auxOpenPopup();
-});
-
-bigEventsPopup.addEventListener("click", (event) => {
-  const classNameOfClickedElement = event.target.classList[0];
-  const classNames = ["close-btn", "popup-overlay", "popup"];
-  const shouldClosePopUp = classNames.some(
-    (className) => className === classNameOfClickedElement
-  );
-
-  if (shouldClosePopUp) {
-    if (window.innerWidth < 1600) {
-      bigEventsMainPopup.style.cssText =
-        "animation:slide-out .5s; animation-fill-mode: forwards";
-    } else {
-      bigEventsMainPopup.style.cssText =
-        "animation:slide-out200 .5s; animation-fill-mode: forwards";
-    }
-    setTimeout(() => {
-      bigEventsPopup.style.display = "none";
-      body.style.overflow = "auto";
-      overlay.style.display = "none";
-    }, 500);
-  }
-});
+};
